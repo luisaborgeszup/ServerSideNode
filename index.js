6f696ebb69d53a0d7112593c83112d497fe1a46a
luisaborgeszup <luisa.borges@zup.com.br>
2019-09-16 16:05:25 -0300

    Trying to complete challenge

---
+++ index.js
@@ -0,0 +1,11 @@
+function saveUsername (e) {
+    const value = e.target.value
+    const keyCode = e.which || e.keyCode
+    const ENTER = 13
+
+    if (keyCode === ENTER) {
+        const username = value
+    }
+}
+
+console.log(username)
